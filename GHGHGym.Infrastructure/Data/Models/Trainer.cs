1c785bf845b1407afdb0e2cf0371e1024a473aaf
Иксан <iksan168@abv.bg>
2022-10-05 15:31:30 +0300

    First model defined

---
+++ GHGHGym.Infrastructure/Data/Models/Trainer.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace GHGHGym.Infrastructure.Data.Models
+{
+    [Comment("Trainers in the gym")]
+    public class Trainer
+    {
+        [Key]
+        [Comment("Primary key")]
+        public Guid Id { get; set; }
+        [Required]
+        [MaxLength(25)]
+        [Comment("First name of the trainer")]
+        public string FirstName { get; set; } = null!;
+        [MaxLength(25)]
+        [Comment("Last name of the trainer")]
+        public string LastName { get; set; } = null!;
+        [Required]
+        [Comment("Trainer add date")]
+        public DateTime AddedOn { get; set; }
+        [Comment("Trainer removal date")]
+        public DateTime? RemovedOn { get; set; }
+        [Required]
+        [Comment("Is the entity deleted from the database")]
+        public bool IsDeleted { get; set; } = false;
+    }
+}
---
+++ GHGHGym/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GHGHGym.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
