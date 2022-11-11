ac5e7e5e3f840db6fa36806f2deb55d1d5ce1542
Иксан <iksan168@abv.bg>
2022-11-11 15:23:23 +0200

    Implemented Repository

---
+++ GHGHGym.Infrastructure/Data/Common/Repositories/Contracts/IRepository.cs
@@ -0,0 +1,34 @@
+﻿using GHGHGym.Infrastructure.Abstractions.Contracts;
+using System.Linq.Expressions;
+
+namespace GHGHGym.Infrastructure.Data.Common.Repositories.Contracts
+{
+    public interface IRepository<TEntity> : IDisposable
+        where TEntity : class, IDeletableEntity
+    {
+        IQueryable<TEntity> All();
+        IQueryable<TEntity> AllExpression(Expression<Func<TEntity, bool>> search);
+        IQueryable<TEntity> AllReadonly();
+        IQueryable<TEntity> AllReadonlyExpression<T>(Expression<Func<TEntity, bool>> search);
+        IQueryable<TEntity> AllWithDeleted();
+        IQueryable<TEntity> AllReadonlyWithDeleted();
+
+        Task<TEntity> GetByIdAsync(object id);
+        Task<TEntity> GetByIdsAsync(object[] id);
+
+        void HardDelete(TEntity entity);
+        void SetDeleted(TEntity entity);
+        Task SetDeletedByIdAsync(object id);
+        void SetDeletedRange(IEnumerable<TEntity> entities);
+        void SetDeletedRange(Expression<Func<TEntity, bool>> deleteWhereClause);
+        void Undelete(TEntity entity);
+
+        Task AddAsync(TEntity entity);
+        Task AddRangeAsync(IEnumerable<TEntity> entities);
+
+        void Update(TEntity entity);
+        void UpdateRange(IEnumerable<TEntity> entities);
+
+        Task<int> SaveChangesAsync();
+    }
+}
---
+++ GHGHGym.Infrastructure/Data/Common/Repositories/Repository.cs
@@ -0,0 +1,127 @@
+﻿using GHGHGym.Infrastructure.Abstractions.Contracts;
+using GHGHGym.Infrastructure.Data.Common.Repositories.Contracts;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace GHGHGym.Infrastructure.Data.Common.Repositories
+{
+    public class Repository<TEntity> : IRepository<TEntity>
+        where TEntity : class, IDeletableEntity
+    {
+
+        public Repository(ApplicationDbContext context)
+        {
+            Context = context ?? throw new ArgumentNullException(nameof(context));
+            DbSet = Context.Set<TEntity>();
+        }
+
+        protected DbSet<TEntity> DbSet { get; set; }
+        protected ApplicationDbContext Context { get; set; }
+
+        public async Task AddAsync(TEntity entity)
+            => await DbSet.AddAsync(entity);
+
+        public async Task AddRangeAsync(IEnumerable<TEntity> entities)
+            => await DbSet.AddRangeAsync(entities);
+        public IQueryable<TEntity> All()
+            => DbSet
+            .Where(x => !x.IsDeleted)
+            .AsQueryable();
+
+        public IQueryable<TEntity> AllExpression(Expression<Func<TEntity, bool>> search)
+            => DbSet.Where(search);
+
+        public IQueryable<TEntity> AllReadonly()
+            => DbSet
+            .Where(x => !x.IsDeleted)
+            .AsNoTracking();
+
+        public IQueryable<TEntity> AllReadonlyExpression<T>(Expression<Func<TEntity, bool>> search)
+            => DbSet
+            .Where(search)
+            .AsNoTracking();
+
+        public IQueryable<TEntity> AllReadonlyWithDeleted()
+            => DbSet
+            .Where(x => !x.IsDeleted)
+            .AsNoTracking();
+
+        public IQueryable<TEntity> AllWithDeleted()
+            => DbSet.AsQueryable();
+
+        public void Dispose()
+        {
+            Context.Dispose();
+            GC.SuppressFinalize(this);
+        }
+
+        public async Task<TEntity> GetByIdAsync(object id)
+            => await DbSet.FindAsync(id) ?? throw new ArgumentNullException();
+
+        public async Task<TEntity> GetByIdsAsync(object[] id)
+            => await DbSet.FindAsync(id) ?? throw new ArgumentNullException();
+
+        public void HardDelete(TEntity entity)
+            => DbSet.Remove(entity);
+
+        public async Task<int> SaveChangesAsync()
+            => await Context.SaveChangesAsync();
+
+        public void SetDeleted(TEntity entity)
+        {
+            entity.IsDeleted = true;
+            entity.DeletedOn = DateTime.UtcNow;
+            Update(entity);
+        }
+
+        public async Task SetDeletedByIdAsync(object id)
+        {
+            var entity = await DbSet.FindAsync(id);
+            if (entity == null)
+            {
+                throw new ArgumentNullException(nameof(entity));
+            }
+            entity.IsDeleted = true;
+            entity.DeletedOn = DateTime.UtcNow;
+            Update(entity);
+        }
+
+        public void SetDeletedRange(IEnumerable<TEntity> entities)
+        {
+            foreach (var entity in entities)
+            {
+                entity.IsDeleted = true;
+                entity.DeletedOn = DateTime.UtcNow;
+            }
+            UpdateRange(entities);
+        }
+
+        public void SetDeletedRange(Expression<Func<TEntity, bool>> deleteWhereClause)
+        {
+            var entities = DbSet.Where(deleteWhereClause);
+            foreach (var entity in entities)
+            {
+                entity.IsDeleted = true;
+                entity.DeletedOn = DateTime.UtcNow;
+            }
+            UpdateRange(entities);
+        }
+
+        public void Undelete(TEntity entity)
+        {
+            entity.IsDeleted = false;
+            entity.DeletedOn = null;
+            Update(entity);
+        }
+
+        public void Update(TEntity entity)
+        {
+            DbSet.Update(entity);
+        }
+
+        public void UpdateRange(IEnumerable<TEntity> entities)
+        {
+            DbSet.UpdateRange(entities);
+        }
+    }
+}
